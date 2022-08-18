d383e7cecc351e165d0b2e180a7855dd9b790254
Isai <castanedaisai303@gmail.com>
2022-08-18 14:19:25 -0700

    commit tree c2c3691f909070013a53cc1c14a757234f182d34

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+//const cards = (["Guadalupe", "Ollie", "Aki"], "surprise");
+
+//function writeCards(arrayOfNames, event) {
+   // let cards = []
+  //for (let i = 0; i < arrayOfNames.length; i++) {
+    //console.log(`Thank you, ${arrayOfNames[i]}, for the wonderful surprise gift!`);
+    //cards.push(`Thank you, ${arrayOfNames[i]}, for the wonderful surprise gift!`)
+    //debugger;
+  //}
+
+  //return cards;
+//}
+
+//writeCards(cards);
+
+
+//function countDown() {
+//for (var x=0; x<11; x++)
+//while (x > 0)
+    //console.log()
+//}
+
+let countdown = 10;
+while (countdown > -1) {
+    console.log(countdown--);
+}
